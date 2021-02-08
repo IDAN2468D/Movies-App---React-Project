aba86bb96dd3639f601c77be6c22619f98f83bc5
IDAN <idankzm@gmai.com>
2021-02-09 00:45:41 +0200

    Update

--- my-app/src/components/HeroSection/HeroSection.js
+++ my-app/src/components/HeroSection/HeroSection.js
@@ -39,11 +39,7 @@ const HeroBg = styled.div`
 
 
 const VideoBg = styled.video`
-    width: 100%;
-    height: 100%;
-    -o-object-fit: cover;
-    object-fit: cover;
-    background: #232a34;
+    
 `;
 
 
